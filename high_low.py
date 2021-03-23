660e90651bb4cb9c20cd459838f897b00e26a0a0
TS22082 <ts22082@gmail.com>
2021-03-23 11:32:58 -0700

    remove redundant code and change .py names

--- high_low.py
+++ high_low.py
@@ -3,20 +3,16 @@ from lib import DHT
 
 with open("/home/pi/Documents/code/grow_lib/data/data.json", "r+") as f:
     data = json.load(f)  # load json from file as dictionary
-    # store current lowest and highest recorded data
-    # lowest_tmp_recorded = data["lowest_temp"]
-    # highest_tmp_recorded = data["highest_temp"]
-    # read data from sensors and save
     current_data = DHT.data()
 
-    # initialise an object with starting values
-    # data_updated = {"lowest_temp": lowest_tmp_recorded,
-    #                 "highest_temp": highest_tmp_recorded}
-
+    #compare current lowest temp with previously recorded lowest temp
     if (current_data["tmp_f"] < data["lowest_temp"]):
+        #overwrite with current lowest temp
         data["lowest_temp"] = current_data["tmp_f"]
 
+    #compare current highest temp with previously recorded highest temp
     if (current_data["tmp_f"] > data["highest_temp"]):
+        #overwrite with current highest temp
         data["highest_temp"] = current_data["tmp_f"]
 
     f.seek(0)  # set cursor back to index 0 in file
