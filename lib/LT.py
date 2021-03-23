1afd2080f1467577b3a4cb61cc8a8375286e6075
TS22082 <ts22082@gmail.com>
2021-03-22 17:01:57 -0700

    format for readability

--- lib/LT.py
+++ lib/LT.py
@@ -8,20 +8,22 @@ sensor = adafruit_tsl2561.TSL2561(i2c)
 sensor.gain = 0
 sensor.integration_time = 1
 
+
 def data():
-	return {
-		"lux" : sensor.lux, 
-		"broadband" : sensor.broadband, 
-		"infrared" : sensor.infrared, 
-		"luminosity" : sensor.luminosity
-		}
+    return {
+        "lux": sensor.lux,
+        "broadband": sensor.broadband,
+        "infrared": sensor.infrared,
+        "luminosity": sensor.luminosity
+    }
+
 
 def display_data():
-  lt_data = data()
-  
-  print("\n*** Data from tsl2561 sensor ***\n")
-  print(f'Lux: {lt_data.get("lux")}')
-  print(f'Broadband: {lt_data.get("broadband")}')
-  print(f'Infrared: {lt_data.get("infrared")}')
-  print(f'Luminosity: {lt_data.get("luminosity")}')
-  print("\n-----------------------------")
+    lt_data = data()
+
+    print("\n*** Data from tsl2561 sensor ***\n")
+    print(f'Lux: {lt_data.get("lux")}')
+    print(f'Broadband: {lt_data.get("broadband")}')
+    print(f'Infrared: {lt_data.get("infrared")}')
+    print(f'Luminosity: {lt_data.get("luminosity")}')
+    print("\n-----------------------------")
